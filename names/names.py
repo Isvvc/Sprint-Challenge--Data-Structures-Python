1481443c19ffc42b695185688de50528ab4e3a01
Isvvc <lyons@tuta.io>
2020-03-27 10:36:30 -0600

    Complete names stretch goal

--- names/names.py
+++ names/names.py
@@ -1,8 +1,6 @@
 import time
 from binary_search_tree import BinarySearchTree
 
-start_time = time.time()
-
 f = open('names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
 f.close()
@@ -13,7 +11,9 @@ f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
-# Replace the nested for loops below with your improvements
+# My solution using a binary search tree
+
+start_time = time.time()
 
 tree = BinarySearchTree(names_1[0])
 names_1.pop(0)
@@ -26,9 +26,21 @@ for name in names_2:
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print (f"Binary search tree runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+duplicates = []
+start_time2 = time.time()
+
+for name in names_2:
+    if name in names_1:
+        duplicates.append(name)
+
+end_time = time.time()
+print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+print (f"Python list runtime: {end_time - start_time2} seconds")
+
